23d4765909e23305c4549e02b80d54d3cf32dc52
Rdyx <tiranico.pro@gmail.com>
2017-07-10 14:14:33 +0200

    base

---
+++ app.js
@@ -0,0 +1,13 @@
+/*$(".buttonlist").click(function (){
+	/*var moreLi = document.createElement("li");
+	var saisie = document.getElementById("list");
+	var text = document.createTextNode(saisie);
+
+	moreLi.appendChild(saisie);
+	var newlist = document.createElement("li").value;
+	var get = document.getElementById("list");
+	var text = document.createTextNode(newlist);
+
+	get.appendChild(text);
+});*/
+
